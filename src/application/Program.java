3d14f5623f9f6c0e88ee468ba9f5521d21a8bf93
rdfarias@zipmail.com.br <Adminstrador@Lenovo-PC>
2019-10-16 00:22:42 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
 	    Seller newSeller = new Seller(null,"Greg", "gregmail@gmail.com", new Date() , 4000.00, department);
 	    sellerDao.insert(newSeller);
 	    System.out.println("Inserted! New id = " + newSeller.getId());
+	    
+	    System.out.println("\n=== TEST 5: seller update ===");
+	    seller = sellerDao.findById(16);
+	    seller.setName("Martha Waine");
+	    sellerDao.update(seller);
+	    System.out.println("Update completed");
 	}
 }
